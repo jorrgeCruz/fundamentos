9705fc1841edd01e2ff689858ba27f1bfe7e96e0
Jorge Cruz <jorrge.cruz@gmail.com>
2023-03-08 11:53:11 -0600

     Mi primer practica con js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//
 function verificarFrase(evt){
+    
     evt.preventDefault();
     let frase = document.getElementById("frase").value;
+    //console.log(frase)
     document.getElementById("salida").innerHTML =  "<p>La frase escrita: <strong>" + frase + "</strong> es o no es palindromo</p>";
 
 }
