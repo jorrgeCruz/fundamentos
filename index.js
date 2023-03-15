058d8c180b1136bb1c1eefcbf557b00c62b1fd65
Jorge Cruz <jorrge.cruz@gmail.com>
2023-03-15 11:43:28 -0600

    Esta es la practica 3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-//
-function verificarFrase(evt){
-    
+function insertarElemento(evt){
     evt.preventDefault();
-    let frase = document.getElementById("frase").value;
-    //console.log(frase)
-    document.getElementById("salida").innerHTML =  "<p>La frase escrita: <strong>" + frase + "</strong> es o no es palindromo</p>";
+    let cadenaArreglo = document.getElementById("array").value;
+    let elemento = document.getElementById("elemento").value;
+    let indice = document.getElementById("indice").value;
+    indice = parseInt(indice);
+    let arreglo = cadenaArreglo.split(',');
+    console.log(arreglo, elemento, indice);
+    document.getElementById("salida").innerHTML =  "<p> el arreglo nuevo es" + arreglo +  "</p>";
 
 }
 
-document.getElementById("check").addEventListener("click", verificarFrase, false);
+document.getElementById("check").addEventListener("click", insertarElemento, false);
